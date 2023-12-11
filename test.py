384bd6959fe614094cc97838d32b7222c04b1e17
조인식 <imsif42@naver.com>
2023-12-11 22:40:43 +0900

    feat: 미디어파이프 추가

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import pygame
 from pygame import mixer
 import random
 import math
+
+import cv2
 import mediapipe as mp
 
 #게임 시작
@@ -13,6 +15,15 @@ screen = pygame.display.set_mode((800,600))
 #제목
 pygame.display.set_caption("Space Invaders")
 
+#그리기 도구 지원해주는 서브 패키지
+mp_drawing = mp.solutions.drawing_utils
+
+#손 감지 모듈
+mp_hands = mp.solutions.hands
+
+# 캠 키기
+cap = cv2.VideoCapture(0)
+
 #점수
 score_value = 0
 font = pygame.font.Font('freesansbold.ttf',45)
@@ -21,6 +32,9 @@ textX = 300
 textY = 20
 
 game_over = pygame.font.Font('freesansbold.ttf',65)
+
+#프레임
+clock = pygame.time.Clock()
 
 #점수 표시
 def display_font(x,y):
@@ -98,75 +112,155 @@ def Collusion(aX,aY,bX,bY):
         return True
     else:
         return False
-#게임 루프
-while True:
-    screen.fill((0,0,0))
-    screen.blit(background,(0,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-            
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -5
-            
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 5
-            
-            if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
-                    bulletX = playerX
-                    fire_bullet(bulletX,bulletY)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+    
+with mp_hands.Hands(max_num_hands = 1, min_detection_confidence =0.5,
+                    min_tracking_confidence = 0.5) as hands:
+    while cap.isOpened():
+
+        clock.tick(60)
+
+        # 캠 읽기 성공여부 success와 읽은 이미지를 image에 저장
+        success, image = cap.read()
+
+        # 캠 읽기 실패시 continue
+        if not success:
+            continue
+
+        # 이미지 값 좌우반전 ( 캠 켰을때 반전된 이미지 보완 )
+        # 이미지 값 순서를 BGR -> RGB로 변환
+        # 이미지 순서가 RGB여야 Mediapipe 사용가능
+        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+
+        # Image에서 손을 추적하고 결과를 result에 저장
+        result = hands.process(image)
+
+        # 이미지 값 순서를 RGB에서 BGR로 다시 바꿈
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        #캠 화면에 띄울 텍스트 정의 ( 기본 값 )
+        gesture_text = 'Cant found hand'
+
+        # 결과 result가 제대로 추적이 되었을때 
+        if result.multi_hand_landmarks:
+
+            # 첫 번째로 추적된 손을 hand_landmarks에 할당
+            hand_landmarks = result.multi_hand_landmarks[0]
+
+            # 검지 ~ 소지 까지의 다 펴져있는지에 대한 bool 변수들 선언
+            finger_1 = False
+            finger_2 = False
+            finger_3 = False
+            finger_4 = False
+            finger_5 = False
+
+            #4번 마디가 2번 마디 보다 y값이 작으면 finger_1를 참
+            if(hand_landmarks.landmark[4].y < hand_landmarks.landmark[2].y):
+                finger_1 = True
+
+            #8번 마디가 6번 마디 보다 y값이 작으면 finger_2를 참
+            if(hand_landmarks.landmark[8].y < hand_landmarks.landmark[6].y):
+                finger_2 = True
+                
+            #12번 마디가 10번 마디 보다 y값이 작으면 finger_3를 참
+            if(hand_landmarks.landmark[12].y < hand_landmarks.landmark[10].y):
+                finger_3 = True
+                
+            #16번 마디가 14번 마디 보다 y값이 작으면 finger_4를 참
+            if(hand_landmarks.landmark[16].y < hand_landmarks.landmark[14].y):
+                finger_4 = True
+                
+            #20번 마디가 18번 마디 보다 y값이 작으면 finger_5를 참
+            if(hand_landmarks.landmark[20].y < hand_landmarks.landmark[18].y):
+                finger_5 = True
+
+            # 검지, 중지, 약지 펴져있으면 STOP
+            if(finger_2 and finger_3 and finger_4):
+                gesture_text = 'stop'
                 playerX_change = 0
-
-    #Player 움직임과 경계설정
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 750:
-        playerX = 750
-        
-    playerX += playerX_change
-    
-    #탄환 움직임
-    if bullet_state is "fire":
-        fire_bullet(bulletX,bulletY)
-        bulletY -= bulletY_change
-    
-    if bulletY <= 0:
-        bullet_state = "ready"
-        bulletY = 480
-
-    #Enemy 이동 & 충돌
-    
-    for i in range(0,6):
-        if enemyY[i]>=480:
-            for j in range(0,6):
-                enemyY[j]= 2000
-            display_gameover()
-            break
             
-        enemyX[i] += enemyX_change[i] 
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 2
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 750:
-            enemyX_change[i] = -2
-            enemyY[i] += enemyY_change[0]
+           
+            
+
+        #캠 화면 ( 이미지 )을 화면에 띄움
+        cv2.imshow('image', image)
+
         
-        colide = Collusion(enemyX[i],enemyY[i],bulletX,bulletY)
+        screen.fill((0,0,0))
+        screen.blit(background,(0,0))
+
         
-        enemy(enemyX[i],enemyY[i],i)
-    
-        if colide:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.display.quit()
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -10
+                
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 10
+                
+                if event.key == pygame.K_SPACE:
+                    if bullet_state is "ready":
+                        bulletX = playerX
+                        fire_bullet(bulletX,bulletY)
+
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+                    
+        
+
+
+        #Player 이동
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 750:
+            playerX = 750
+            
+        playerX += playerX_change
+        
+        #Bullet 이동
+        if bullet_state is "fire":
+            fire_bullet(bulletX,bulletY)
+            bulletY -= bulletY_change
+        
+        if bulletY <= 0:
             bullet_state = "ready"
             bulletY = 480
-            enemyX[i] = random.randint(0,755)
-            enemyY[i]= random.randint(50,200)
-            score_value +=1
-              
+        
+        #Enemy 이동
+        
+        for i in range(0,3):
+            if enemyY[i]>=480:
+                for j in range(0,3):
+                    enemyY[j]= 2000
+                display_gameover()
+                break
+                
+            enemyX[i] += enemyX_change[i] 
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 4
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 750:
+                enemyX_change[i] = -4
+                enemyY[i] += enemyY_change[0]
+            
+            colide = Collusion(enemyX[i],enemyY[i],bulletX,bulletY)
+            
+            enemy(enemyX[i],enemyY[i],i)
+        
+            if colide:
+                bullet_state = "ready"
+                bulletY = 480
+                enemyX[i] = random.randint(0,755)
+                enemyY[i]= random.randint(50,200)
+                score_value +=1
 
-    display_font(textX,textY)
-    player(playerX,playerY)       
-    pygame.display.update()
+        display_font(textX,textY)
+        player(playerX,playerY)       
+        pygame.display.update()
+
+   
+    
